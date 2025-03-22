03df73cad4fdcd134e5a70c24ef489b19807159d
NickCoffin123 <nicholas.coffin@dcmail.ca>
2025-03-22 12:47:19 -0400

    stuff and things

--- DRTApp/MainPage.xaml.cs
+++ DRTApp/MainPage.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace DRTApp
+﻿using ProtoBuf;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using TransitRealtime;
+
+namespace DRTApp
 {
     public partial class MainPage : ContentPage
     {
